0254eaf0840d6214ab0eb82408f08f1ddef6b73f
alwahid786 <wahidforjob@gmail.com>
2025-06-05 12:15:13 +0500

    complet the 2nd stepper

--- src/components/applicationVerification/CompanyInformation.jsx
+++ src/components/applicationVerification/CompanyInformation.jsx
@@ -7,12 +7,29 @@ import Modal2 from './companyInfo/Modal2';
 import Modal3 from './companyInfo/Modal3';
 import Modal4 from './companyInfo/Modal4';
 import Modal5 from './companyInfo/Modal5';
+import { Ai } from '@/assets/svgs/icon';
 
 function CompanyInformation({ data, updateField, index }) {
   const [activeModal, setActiveModal] = useState(null);
   const [businessDescription, setBusinessDescription] = useState(false);
   const [businessClassification, setBusinessClassification] = useState(false);
+  const [showInput1, setShowInput1] = useState(false);
+  const options = [
+    { label: 'Limited Liability Company (LLC)', id: 'llc' },
+    { label: 'Corporation (Inc.)', id: 'inc' },
+    { label: 'Limited Partnership (LTD)', id: 'ltd' },
+    { label: 'Sole Proprietorship', id: 'Sole-Proprietorship' },
+    { label: 'Non-Profit', id: 'Non-Profit' },
+    // { label: 'Custom', id: 'Custom' },
+  ];
+  const ownerOptions = [
+    { label: 'Private', id: 'private' },
 
+    { label: 'Public', id: 'Public' },
+  ];
+  const handleRentReasonChange1 = e => {
+    setShowInput1(e.target.id === 'Public');
+  };
   const openModal = modalNumber => {
     setActiveModal(modalNumber);
   };
@@ -48,15 +65,14 @@ function CompanyInformation({ data, updateField, index }) {
       case 2:
         return <Modal2 modal1Handle={nextModal} />;
       case 3:
-        return <Modal3 modal1Handle={nextModal} />;
-
+        return <Modal3 modal1Handle={closeModal} />;
       default:
         return null;
     }
   };
 
   return (
-    <div className="mt-14">
+    <div className="mt-14 h-full overflow-auto">
       <div className="rounded-lg border border-gray-300 p-6">
         <h2 className="text-xl font-medium">Confirm Your Information</h2>
         <h5 className="text-textSecondary text-base">Please enter your company information</h5>
@@ -68,6 +84,109 @@ function CompanyInformation({ data, updateField, index }) {
             className="h-[45px] flex-none !border-[#A7A7A7] !bg-white !text-[#A7A7A7]"
             label={'Look Up'}
           />
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="my-5 flex w-full flex-col items-start">
+          <h1 className="text-xl font-medium">Basic Information</h1>
+          <div className="mt-6 flex w-full flex-col gap-4">
+            <TextField label={'Website URL'} />
+            <TextField label={'Legal Company Name'} />
+            <TextField label={'DBA Name'} />
+          </div>
+          <div className="w-full">
+            <div className="w-full">
+              <div className="mt-6 flex w-full justify-end">
+                <Button
+                  label={'AI Help'}
+                  onClick={openBusinessDescriptionHandel}
+                  icon={Ai}
+                  className="!border-none !bg-[#F5F5F5] !text-[#1A1A1A] hover:!bg-gray-500"
+                />
+              </div>
+              <TextField label={'Business Description'} />
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="w-full">
+              <div className="mt-6 flex w-full justify-end">
+                <Button
+                  label={'AI Help'}
+                  onClick={openBusinessClassificationHandel}
+                  icon={Ai}
+                  className="!border-none !bg-[#F5F5F5] !text-[#1A1A1A] hover:!bg-gray-500"
+                />
+              </div>
+              <TextField label={'Business Classification'} />
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full flex-col justify-start">
+          <h1 className="text-lg font-medium">Legal Entity Type</h1>
+          <div className="border-b-2 py-6">
+            <div className="grid grid-cols-2 gap-4">
+              {options.map(({ id, label }) => (
+                <div key={id} className="flex items-center gap-2 p-2">
+                  <input
+                    className="size-5"
+                    id={id}
+                    type="radio"
+                    name="rentReason1"
+                    onChange={handleRentReasonChange1}
+                  />
+                  <label className="text-base" htmlFor={id}>
+                    {label}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+          <h1 className="mt-6 text-lg font-medium">Company Ownership Type</h1>
+          <div className="border-b-2 py-6">
+            <div className="grid grid-cols-3 gap-4">
+              {ownerOptions.map(({ id, label }) => (
+                <div key={id} className="flex items-center gap-2 p-2">
+                  <input
+                    className="size-5"
+                    id={id}
+                    type="radio"
+                    name="rentReason1"
+                    onChange={handleRentReasonChange1}
+                  />
+                  <label className="text-base" htmlFor={id}>
+                    {label}
+                  </label>
+                </div>
+              ))}
+              {showInput1 && (
+                <div>
+                  <div className="mt-1">
+                    <TextField type="text" placeholder="AAPL (NASDAQ)" />
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="mt-6">
+            <TextField label={'SSN (Social Security Number)'} />
+            <h5>Enter your Social Security Number (XXX-XX-XXXX)</h5>
+          </div>
+          {/* <div className="flex justify-end gap-4">
+            <Button label={'Back'} variant="secondary" />
+            <Button label={'Next'} onClick={modal1Handle} />
+          </div> */}
+        </div>
+        <div className="flex w-full flex-col gap-4">
+          <h1 className="text-lg font-medium">Legal Entity Type</h1>
+          <TextField label={'Street Address'} />
+          <TextField label={'Apt/Suite/Unit'} />
+          <TextField label={'ZIP code'} />
+          <TextField label={'Country'} />
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <TextField label={'City'} />
+            <TextField label={'State'} />
+          </div>
+          <TextField label={'Company Phone'} />
         </div>
       </div>
       {activeModal && <Modal onClose={closeModal}>{renderModal()}</Modal>}
--- src/components/applicationVerification/companyInfo/Modal3.jsx
+++ src/components/applicationVerification/companyInfo/Modal3.jsx
@@ -1,7 +1,33 @@
+import Button from '@/components/shared/small/Button';
+import TextField from '@/components/shared/small/TextField';
 import React from 'react';
 
 function Modal3({ modal1Handle }) {
-  return <div>Modal3</div>;
+  return (
+    <div className="m-2 flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center">
+        <h1 className="text-primary text-4xl font-extrabold">LOGO</h1>
+        <h2 className="text-xl font-medium">Confirm Your Information</h2>
+        <h5 className="text-textSecondary text-base">Please enter your company information</h5>
+      </div>
+      <div className="flex w-full flex-col gap-4">
+        <h1 className="text-lg font-medium">Legal Entity Type</h1>
+        <TextField label={'Street Address'} />
+        <TextField label={'Apt/Suite/Unit'} />
+        <TextField label={'ZIP code'} />
+        <TextField label={'Country'} />
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <TextField label={'City'} />
+          <TextField label={'State'} />
+        </div>
+        <TextField label={'Company Phone'} />
+      </div>
+      <div className="mt-6 flex w-full justify-end gap-4">
+        <Button label={'Back'} variant="secondary" />
+        <Button label={'Next'} onClick={modal1Handle} />
+      </div>
+    </div>
+  );
 }
 
 export default Modal3;
