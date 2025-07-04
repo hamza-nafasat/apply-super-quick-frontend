5684293035c0a8a1d54a9e0d9d3329fc5e55cb50
alwahid786 <wahidforjob@gmail.com>
2025-07-04 18:38:34 +0500

    fix the name of branding

--- src/page/admin/layout/AdminAside.jsx
+++ src/page/admin/layout/AdminAside.jsx
@@ -37,7 +37,7 @@ const AdminAside = () => {
       icon: <Applicants />,
     },
     {
-      title: 'Brandings',
+      title: 'Branding',
       link: '/branding',
       icon: <HiOutlineLightBulb />,
     },
