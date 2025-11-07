70b45a0e1fef5c916889c649bcd5b672dc682df3
muhammadnisarbsse7 <muhammadnisarbsse7@gmail.com>
2025-11-07 19:23:22 +0500

    push

--- src/page/admin/layout/AdminAside.jsx
+++ src/page/admin/layout/AdminAside.jsx
@@ -34,7 +34,7 @@ const AdminAside = ({ sidebarOpen, setSidebarOpen }) => {
     { title: 'Lookup management', link: '/strategies-key', icon: <HiOutlineLightBulb /> },
     { title: 'Verification', link: '/verification-test', icon: <CheckCircle /> },
     { title: 'Strategies', link: '/strategies', icon: <PiStrategyBold /> },
-    { title: 'Pdf View', link: 'singleform/pdf-view/:pdfId', icon: <RiHistoryLine size={20} /> },
+    { title: 'Pdf View', link: 'singleform/pdf-view/6904a7e037da446e85b46a4d', icon: <RiHistoryLine size={20} /> },
   ];
 
   return (
