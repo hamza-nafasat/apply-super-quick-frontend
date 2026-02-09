edfebf2f1054090c663bbe57d09054d64aada037
hamza-nafasat <hamzanafasat4155@gmail.com>
2026-02-09 18:07:49 +0500

    fix

--- src/page/admin/dashboard/brandings/Brandings.jsx
+++ src/page/admin/dashboard/brandings/Brandings.jsx
@@ -1,26 +1,26 @@
-import ApplyBranding from '@/components/admin/brandings/globalBranding/ApplyBranding';
-import ConfirmationModal from '@/components/shared/ConfirmationModal';
-import Button from '@/components/shared/small/Button';
-import CustomLoading from '@/components/shared/small/CustomLoading';
-import { ThreeDotEditViewDelete } from '@/components/shared/ThreeDotViewEditDelete';
+import ApplyBranding from "@/components/admin/brandings/globalBranding/ApplyBranding";
+import ConfirmationModal from "@/components/shared/ConfirmationModal";
+import Button from "@/components/shared/small/Button";
+import CustomLoading from "@/components/shared/small/CustomLoading";
+import { ThreeDotEditViewDelete } from "@/components/shared/ThreeDotViewEditDelete";
 // import { Button } from '@/components/ui/button';
-import { getTableStyles } from '@/data/data';
-import { useBranding } from '@/hooks/BrandingContext';
-import { useGetMyProfileFirstTimeMutation } from '@/redux/apis/authApis';
+import { getTableStyles } from "@/data/data";
+import { useBranding } from "@/hooks/BrandingContext";
+import { useGetMyProfileFirstTimeMutation } from "@/redux/apis/authApis";
 import {
   useAddBrandingInFormMutation,
   useDeleteSingleBrandingMutation,
   useGetAllBrandingsQuery,
-} from '@/redux/apis/brandingApis';
-import { useGetMyAllFormsQuery } from '@/redux/apis/formApis';
-import { userExist, userNotExist } from '@/redux/slices/authSlice';
-import { MoreVertical, Pencil, Trash } from 'lucide-react';
-import React, { useEffect, useRef, useState } from 'react';
-import DataTable from 'react-data-table-component';
-import { FaExchangeAlt } from 'react-icons/fa';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+} from "@/redux/apis/brandingApis";
+import { useGetMyAllFormsQuery } from "@/redux/apis/formApis";
+import { userExist, userNotExist } from "@/redux/slices/authSlice";
+import { MoreVertical, Pencil, Trash } from "lucide-react";
+import React, { useEffect, useRef, useState } from "react";
+import DataTable from "react-data-table-component";
+import { FaExchangeAlt } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Brandings = () => {
   const dispatch = useDispatch();
@@ -51,40 +51,48 @@ const Brandings = () => {
     setFrameColor,
     setFontFamily,
     setLogo,
+    setButtonTextPrimary,
+    setButtonTextSecondary,
+    setHeaderAlignment,
+    setHeaderBackground,
+    setFooterBackground,
+    setHeaderText,
+    setFooterText,
+    setHighlightingColor,
   } = useBranding();
 
   const ButtonsForThreeDot = [
     {
-      name: 'edit',
+      name: "edit",
       icon: <Pencil size={16} className="mr-2" />,
-      onClick: row => {
+      onClick: (row) => {
         navigate(`/branding/single/${row?._id}`);
         setActionMenu(null);
       },
     },
     {
-      name: 'delete',
+      name: "delete",
       icon: <Trash size={16} className="mr-2" />,
       disabled: isDeleting,
-      onClick: async row => {
+      onClick: async (row) => {
         try {
-          if (!row?._id) toast.error('Branding ID is missing');
+          if (!row?._id) toast.error("Branding ID is missing");
           const res = await deleteBranding(row?._id).unwrap();
           if (res.success) {
             await refetch();
-            toast.success(row?.message || 'Branding deleted successfully');
+            toast.success(row?.message || "Branding deleted successfully");
           }
         } catch (error) {
-          toast.error(error?.data?.message || 'Failed to delete branding');
+          toast.error(error?.data?.message || "Failed to delete branding");
         } finally {
           setActionMenu(null);
         }
       },
     },
     {
-      name: 'apply',
+      name: "apply",
       icon: <FaExchangeAlt size={16} className="mr-2" />,
-      onClick: row => {
+      onClick: (row) => {
         setApplyModal(true);
         setSelectedBranding(row?._id);
         setActionMenu(null);
@@ -93,15 +101,15 @@ const Brandings = () => {
   ];
 
   const onConfirmApply = async () => {
-    if (!selectedBranding) toast.error('Branding ID is missing');
-    if (!selectedId && !onHome) toast.error('Form ID is required if onHome is not provided');
+    if (!selectedBranding) toast.error("Branding ID is missing");
+    if (!selectedId && !onHome) toast.error("Form ID is required if onHome is not provided");
     try {
       const res = await addFromBranding({
         brandingId: selectedBranding,
         formId: selectedId,
-        onHome: onHome ? 'yes' : 'no',
+        onHome: onHome ? "yes" : "no",
       }).unwrap();
-      console.log('res', res);
+      console.log("res", res);
       if (res?.success) {
         if (onHome) {
           const res = await getUserProfile().unwrap();
@@ -117,10 +125,18 @@ const Brandings = () => {
               setFrameColor(userBranding.colors.frame);
               setFontFamily(userBranding.fontFamily);
               setLogo(userBranding?.selectedLogo);
+              setButtonTextPrimary(userBranding?.buttonTextPrimary);
+              setButtonTextSecondary(userBranding?.buttonTextSecondary);
+              setHeaderAlignment(userBranding?.headerAlignment);
+              setHeaderBackground(userBranding?.headerBackground);
+              setFooterBackground(userBranding?.footerBackground);
+              setHeaderText(userBranding?.headerText);
+              setFooterText(userBranding?.footerText);
+              setHighlightingColor(userBranding?.highlightingColor);
             }
           }
           await getUserProfile()
-            .then(res => {
+            .then((res) => {
               if (res?.data?.success) {
                 dispatch(userExist(res.data.data));
                 console.log(res?.data?.data?.branding);
@@ -129,11 +145,11 @@ const Brandings = () => {
             .catch(() => dispatch(userNotExist()));
         }
         await formRefetch();
-        toast?.success(res?.message || 'Branding applied successfully');
+        toast?.success(res?.message || "Branding applied successfully");
       }
     } catch (error) {
-      console.error('Error applying branding:', error);
-      toast.error(error?.data?.message || 'Failed to apply branding');
+      console.error("Error applying branding:", error);
+      toast.error(error?.data?.message || "Failed to apply branding");
     } finally {
       setApplyModal(false);
       setSelectedId(null);
@@ -143,28 +159,28 @@ const Brandings = () => {
 
   const columns = () => [
     {
-      name: 'Name',
-      selector: row => row?.name,
+      name: "Name",
+      selector: (row) => row?.name,
       sortable: true,
     },
     {
-      name: 'Url',
-      selector: row => row?.url || 'N/A',
+      name: "Url",
+      selector: (row) => row?.url || "N/A",
       sortable: true,
     },
     {
-      name: 'logos',
-      selector: row => row?.logos?.length || 0,
+      name: "logos",
+      selector: (row) => row?.logos?.length || 0,
       sortable: true,
     },
     {
-      name: 'Font family',
-      selector: row => row?.fontFamily || 'N/A',
+      name: "Font family",
+      selector: (row) => row?.fontFamily || "N/A",
       sortable: true,
     },
     {
-      name: 'Action',
-      cell: row => {
+      name: "Action",
+      cell: (row) => {
         if (!actionMenuRefs.current.has(row?._id)) {
           actionMenuRefs.current.set(row?._id, React.createRef());
         }
@@ -172,7 +188,7 @@ const Brandings = () => {
         return (
           <div className="relative" ref={rowRef}>
             <button
-              onClick={() => setActionMenu(prevActionMenu => (prevActionMenu === row?._id ? null : row?._id))}
+              onClick={() => setActionMenu((prevActionMenu) => (prevActionMenu === row?._id ? null : row?._id))}
               className="cursor-pointer rounded p-1 hover:bg-gray-100"
               aria-label="Actions"
             >
@@ -186,15 +202,15 @@ const Brandings = () => {
   ];
 
   useEffect(() => {
-    const handleClickOutside = event => {
+    const handleClickOutside = (event) => {
       const clickedOutsideAllMenus = Array.from(actionMenuRefs.current.values()).every(
-        ref => !ref.current?.contains(event.target)
+        (ref) => !ref.current?.contains(event.target)
       );
       if (clickedOutsideAllMenus) setActionMenu(null);
     };
-    if (actionMenu !== null) document.addEventListener('mousedown', handleClickOutside);
+    if (actionMenu !== null) document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [actionMenu]);
 
@@ -219,11 +235,11 @@ const Brandings = () => {
           cancelButtonText="cancel"
           onConfirm={onConfirmApply}
           onClose={() => setApplyModal(false)}
-          title={'Apply Branding'}
+          title={"Apply Branding"}
         />
       )}
       <div className="mb-4 flex justify-end">
-        <Button label={'Create Branding'} onClick={() => navigate('/branding/create')} />
+        <Button label={"Create Branding"} onClick={() => navigate("/branding/create")} />
         {/* Create Branding
         </Button> */}
       </div>
