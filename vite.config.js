022fcf89a80124a5192c856dafe41b68aeb7c2db
hamza_nafasat <hamzanafasat4155@gmail.com>
2026-06-26 18:52:33 +0500

    push the latest code

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { createRequire } from "module";
+// import { createRequire } from "module";
 
-const require = createRequire(import.meta.url);
+// const require = createRequire(import.meta.url);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
